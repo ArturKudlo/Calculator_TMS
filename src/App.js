91ad6f5f9c2b180216016bb03bfbfb2fd8c34fdf
Artur Kudla <arturkapes@gmail.com>
2019-04-14 23:10:33 +0300

    feat: add battons and install mathjs

--- src/App.js
+++ src/App.js
@@ -8,8 +8,37 @@ import * as math from 'mathjs';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       
+      <div className="app">
+        <div className="calculator-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.handleMultiply}>x</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>-</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.addToInput}>/</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({input: ""})}>clear</ClearButton>
+          </div>
+        </div>
       </div>
     );
   }
